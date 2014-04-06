def405108a5e169a61e001fcf356a6c07ea896d7
Johan Lundström <johan.lundstrom@apoex.se>
2014-04-06 19:22:50 +0200

    extract configuration to ENV

--- config/initializers/airbrake_api.rb
+++ config/initializers/airbrake_api.rb
@@ -1,5 +1,5 @@
-Rails.configuration.airbrake_project_name = "Link Journal (DV)"
-Rails.configuration.airbrake_project_id = 91445 # Link Journal DV
-Rails.configuration.airbrake_account = 'apoex'
-Rails.configuration.airbrake_auth_token = '***REMOVED***'
+Rails.configuration.airbrake_project_name = ENV['AIRBRAKE_PROJECT_NAME']
+Rails.configuration.airbrake_project_id   = ENV['AIRBRAKE_PROJECT_ID']
+Rails.configuration.airbrake_account      = ENV['AIRBRAKE_ACCOUNT']
+Rails.configuration.airbrake_auth_token   = ENV['AIRBRAKE_AUTH_TOKEN']
 
