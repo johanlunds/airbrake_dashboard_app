95b54cbb801cfb3f52ca8bf2d4f85ff330e8f638
Johan Lundström <johan.lundstrom@apoex.se>
2014-04-06 15:35:54 +0200

    rails new airbrake_dashboard_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AirbrakeDashboardApp::Application.config.secret_key_base = '1139cd5df84af2cdcbe3c5ad40aaad01ba686c88129c80e93d7d98b58bff2cd6be8c51b4b53a7313811296b43a598fb07a50a5fb06143b24d1a0a0290a9d4683'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AirbrakeDashboardApp::Application.config.session_store :cookie_store, key: '_airbrake_dashboard_app_session'
